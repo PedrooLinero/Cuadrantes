8fd4454b145173ad27f5e0469072985beaab9030
Pedro Linero <xplinari721@ieshnosmachado.org>
2025-06-24 14:24:32 +0200

    pantalla seleccionable

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Menu from "../components/Menu";
+import CreateCenterScreen from "../components/CrearCentro";
 
 
 function Home() {
   return (
     <>
       <Menu />
+      <CreateCenterScreen />
       <Outlet />
     </>
   );
